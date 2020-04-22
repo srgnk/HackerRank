d555c3a9766a81d3b94fa13ec9744b4e98417896
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-22 11:17:49 +0530

    Update taum-and-bday.py

--- algorithms/taum-and-bday.py
+++ algorithms/taum-and-bday.py
@@ -7,22 +7,8 @@ import sys
 # Complete the taumBday function below.
 #
 def taumBday(b, w, bc, wc, z):
-    res = 0
-    
-    if bc <= wc:
-        res += bc*b
-        if bc + z <= wc:
-            res += (bc + z)*w
-        else:
-            res += wc*w
-    else:
-        res += wc*w
-        if wc + z <= bc:
-            res += (wc + z)*b
-        else:
-            res += bc*b
-        
-    return res
+    # Write your code here
+    print (min(b*bc, b*(wc+z)) + min(w*(bc+z), w*wc))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
