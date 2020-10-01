521ce5453bb2899bfcdfb74a9d99eff381d287c8
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2020-10-01 10:23:23 +0530

    Update functions-in-c.c
    I wrote a code in a good way that helps other programmers to understand in a better way

--- c/functions-in-c.c
+++ c/functions-in-c.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
+#include <stdio.h>  //headerFile
+int max_of_four(int , int , int , int); //function declaration
 
-int max_of_four(int a, int b, int c, int d)
+int max_of_four(int a, int b, int c, int d) //function definition
 {
+    //body of function
     int res = a;
-    
+
     if (b > res)
         res = b;
     if (c > res)
         res = c;
     if (d > res)
         res = d;
-    
+
     return res;
 }
 
-
+//main function
 int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
-    
+    int num_1, num_2, num_3, num_4;
+    printf("Enter FOUR numbers : ");
+    scanf("%d %d %d %d", &num_1, &num_2, &num_3, &num_4);
+    int answer = max_of_four(num_1, num_2, num_3, num_4);
+    printf("\nLargest Number is : %d", answer);
     return 0;
 }
